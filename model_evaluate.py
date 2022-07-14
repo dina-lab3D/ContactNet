043b22d6cf304d8367b03da7c81e08b5520419ed
matanhalfon <matanh1000@gmail.com>
2022-07-14 12:30:46 +0300

    fix trans header

--- model_evaluate.py
+++ model_evaluate.py
@@ -57,7 +57,6 @@ def hit_rate(model,prot_ag,prot_ab,config,data_dir="",trans_num=0):
     try:
         for b in gen:
             scores=model.predict(b)["classification"]
-            print(scores)
             if score_by_label is None:
                 score_by_label=scores
             else:
@@ -68,7 +67,7 @@ def hit_rate(model,prot_ag,prot_ab,config,data_dir="",trans_num=0):
     except EOFError:
         print("finished with "+trans_file )
     gc.collect()
-    trans=pd.read_csv(osp.join("PPI","trans.txt"),sep="\t",index_col=0).to_numpy().reshape((-1,1))[:len(score_by_label)]
+    trans=pd.read_csv(osp.join("PPI","trans.txt"),sep="\t",index_col=0,header=None).to_numpy().reshape((-1,1))[:len(score_by_label)]
     score_by_label = np.concatenate([score_by_label, trans],axis=1)
     df=pd.DataFrame(score_by_label,columns=["score","trans"])
     df.to_csv("evaluation", sep="\t", quoting=csv.QUOTE_NONE,
